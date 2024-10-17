57df736d6ce6715e9d9f374c0e25c152f9a0fd7c
Jao-Viquitor <victorsantos.ft18@gmail.com>
2024-10-17 15:13:36 -0300

    oi

---
+++ server/publications/publications.js
@@ -0,0 +1,10 @@
+import {Meteor} from "meteor/meteor";
+import {Transactions} from "../imports/api/transactions";
+
+Meteor.publish('transactions', function () {
+    if (!this.userId) {
+        return this.ready();
+    }
+
+    return Transactions.find({ userId: this.userId });
+});
